f3dd3a1323c78ab96aba9a401cbca4326e63ba41
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-25 13:01:35 +0545

    chore: Setup folder structure, axios request interceptor, browser router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import AppRoutes from './routes/AppRoutes'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
